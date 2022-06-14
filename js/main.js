008ca1b82cdb9e6e33b6dca85a861f6be3c0201a
ivan-zub <93342329+ivan-zub@users.noreply.github.com>
2022-06-14 18:20:32 +0300

    вносит правки

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
-  const getRandomIntInclusive = (min, max) => {
-    return(Math.round(Math.random() * (max - min + 1)) + min);
-  }
-  getRandomIntInclusive(0, 10);
+const getRandomIntInclusive = (min, max) => (Math.round(Math.random() * (max - min + 1)) + min);
+getRandomIntInclusive(0, 10);
 
-  const checkStringLength = (string, maxLength) => string.length <= maxLength;
+const checkStringLength = (string, maxLength) => string.length <= maxLength;
 
-  checkStringLength(140);
+checkStringLength(140);
